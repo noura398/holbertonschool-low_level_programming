7c3e4476a632cd551f13ca391e6e825710b64929
noura398 <0nourasubaie@gmail.com>
2025-09-01 08:22:42 +0000

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-<TAB>printf("with proper grammar, but the outcome is a piece of art,\n");
-<TAB>return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
