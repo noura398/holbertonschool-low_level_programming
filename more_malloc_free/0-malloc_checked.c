a9c1ff4773a37dc8d7526d7cc49b89c2dd0d8701
noura398 <0nourasubaie@gmail.com>
2025-09-26 16:33:39 +0000

    a

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ * On failure: process terminates with status 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
