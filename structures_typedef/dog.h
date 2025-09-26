e9647ea2ba9caf11ad19789c75619be263db6cb1
noura398 <0nourasubaie@gmail.com>
2025-09-26 16:54:14 +0000

    a

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic dog structure
+ * @name: dog's name (string)
+ * @age: dog's age (in years)
+ * @owner: owner's name (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
