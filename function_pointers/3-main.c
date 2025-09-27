6a420aca8d16e6666593b8be248a4cf026952b1d
noura398 <0nourasubaie@gmail.com>
2025-09-27 15:39:14 +0000

    l

---
+++ function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - simple calculator
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, exits on error
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+	if (argv[2][1] != '\0' || func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(a, b);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum
+ * @a: left operand
+ * @b: right operand
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference
+ * @a: left operand
+ * @b: right operand
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product
+ * @a: left operand
+ * @b: right operand
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - quotient
+ * @a: left operand
+ * @b: right operand
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder
+ * @a: left operand
+ * @b: right operand
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
