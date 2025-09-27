0b1ae973b01b7f526b5e51e17fc394f75ca7db38
noura398 <0nourasubaie@gmail.com>
2025-09-27 16:00:54 +0000

    l

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,52 +3,78 @@
 #include <stdio.h>
 
 /**
+ * print_char - prints a char from va_list
+ * @ap: argument list
+ */
+static void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_int - prints an int from va_list
+ * @ap: argument list
+ */
+static void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - prints a float (as double) from va_list
+ * @ap: argument list
+ */
+static void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - prints a string from va_list (or (nil))
+ * @ap: argument list
+ */
+static void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
  * print_all - prints anything based on a format string
- * @format: list of argument types: 'c', 'i', 'f', 's'
- *
- * Return: void
+ * @format: list of types: 'c', 'i', 'f', 's'
  */
 void print_all(const char * const format, ...)
 {
+	typedef struct spec { char t; void (*f)(va_list); } spec_t;
+	spec_t ops[] = {
+		{'c', print_char}, {'i', print_int},
+		{'f', print_float}, {'s', print_string},
+		{'\0', NULL}
+	};
 	va_list ap;
-	unsigned int i = 0;
-	char *s, *sep = "";
+	unsigned int i = 0, j;
+	char *sep = "";
 
 	va_start(ap, format);
-
-	if (format)
+	while (format && format[i])
 	{
-		while (format[i])
+		j = 0;
+		while (ops[j].t)
 		{
-			switch (format[i])
+			if (ops[j].t == format[i])
 			{
-			case 'c':
-				printf("%s%c", sep, va_arg(ap, int));
+				printf("%s", sep);
+				ops[j].f(ap);
 				sep = ", ";
 				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(ap, int));
-				sep = ", ";
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(ap, double));
-				sep = ", ";
-				break;
-			case 's':
-				s = va_arg(ap, char *);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s%s", sep, s);
-				sep = ", ";
-				break;
-			default:
-				i++;
-				continue;
 			}
-			i++;
+			j++;
 		}
+		i++;
 	}
-
 	printf("\n");
 	va_end(ap);
 }
